123cf1a8bb2e8187df4841fc805c2eaeaa940ee0
Quentin Quéro <quentin.quero@slickteam.fr>
2019-08-30 15:51:26 +0200

    add table

--- portlets/src/main/frontend/apps/functionalConfiguration/functionalConfiguration.js
+++ portlets/src/main/frontend/apps/functionalConfiguration/functionalConfiguration.js
@@ -1,4 +1,11 @@
 import FunctionalConfigurationApp from './components/functionalConfiguration.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Bootstrap from 'bootstrap-vue'
+
+
+Vue.config.productionTip = false
+Vue.use(Bootstrap)
 
 const lang = eXo.env.portal.language;
 const url = `/functional-configuration-portlets/vuesLocales/locale_${lang}.json`;
