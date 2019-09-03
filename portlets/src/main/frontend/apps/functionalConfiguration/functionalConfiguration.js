8e7d98e8085ff554147efefea2b23dd9b6f30194
Quentin Quéro <quentin.quero@slickteam.fr>
2019-09-03 09:57:42 +0200

    change table and translate

--- portlets/src/main/frontend/apps/functionalConfiguration/functionalConfiguration.js
+++ portlets/src/main/frontend/apps/functionalConfiguration/functionalConfiguration.js
@@ -2,10 +2,14 @@ import FunctionalConfigurationApp from './components/functionalConfiguration.vue
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Bootstrap from 'bootstrap-vue'
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 Vue.config.productionTip = false
 Vue.use(Bootstrap)
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 const lang = eXo.env.portal.language;
 const url = `/functional-configuration-portlets/vuesLocales/locale_${lang}.json`;
